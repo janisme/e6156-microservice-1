26cf410b2e60ddc40197266c3e784c9403fcfa52
YC Chen <49497111+janisme@users.noreply.github.com>
2023-11-14 22:41:24 -0500

    Update students.py
    change it back
    
    Signed-off-by: YC Chen <49497111+janisme@users.noreply.github.com>

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/home/ec2-user/e6156-microservice-sprint_0/resources/old-students.json"
+        "/Users/jannetchen/PycharmProjects/e6156-microservice-1/resources/old-students.json"
 
     def __init__(self):
         self.students = None
